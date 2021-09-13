8b49e71c3f78b6d0a12cd064d751eb0058c9090c
Jack Nelson <pl224701@knights.ucf.edu>
2021-09-13 09:46:23 -0400

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,16 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 1 Solution
+ *  Copyright 2021 Jack Nelson
+ */
+
+import java.util.Scanner;
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println("What is your name?");
+        Scanner sc= new Scanner(System.in);
+        String str = sc.nextLine();
+        System.out.printf("Hello, %s, nice to meet you!", str);
+    }
+}
